cf57943fee3f9151f8c789a364d6fc56fef8a00b
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-03 16:57:27 -0600

    Completed removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,target, ...theArgs) {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === target || theArgs.includes(arr[i])){
+            arr.splice(i,1)
+            i--;
+        }
+    }
+    console.log(arr)
+    return arr
+    
+
 };
 
 // Do not edit below this line
