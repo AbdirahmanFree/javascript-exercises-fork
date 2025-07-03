239a2345825bb7f1c5c567eebdccd9bd27c4be9d
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-03 16:11:42 -0600

    Completed repeatString.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = ""
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++){
+        str = str + string;
+    }
+    return str
 
 };
 
