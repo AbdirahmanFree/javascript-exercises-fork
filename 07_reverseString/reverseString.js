fbc35092d9a2a0e80b07f29f332afa9585e24090
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-03 16:21:11 -0600

    Finished Reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    revString = ""
+    for (let i = string.length; i >= 0; i--){
+        revString = revString +  string.charAt(i)
+    }
+    return revString    
 };
 
 // Do not edit below this line
