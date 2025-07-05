561333f59a89832c798ca63fd1e610f3f87bf56b
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-04 18:03:25 -0600

    Finished temp conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let val = ((tempF - 32) *(5/9)) *10
+  return Math.round(val)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let val = (tempC * (9/5)) +32
+  return Math.round(val*10)/10
 };
 
 // Do not edit below this line
