2815ac4a277e7692af71b6b14cc74a2d6e8e8197
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-22 16:28:33 -0700

    completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+	return a-b
+};
+
+const sum = function(numbers) {
+  return numbers.reduce((acc, number) => {
+    return acc + number;
+  },0)
+
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(numbers) {
+  return numbers.reduce((acc,number) => {
+    return acc*number;
+  },1)
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <=1){
+    return 1;
+  }
+  else{
+    return num * factorial(num-1)
+  }
 };
 
 // Do not edit below this line
