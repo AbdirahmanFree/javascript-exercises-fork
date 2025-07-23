49913684af42a61623b3395a3bf28b806700e043
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-23 15:27:05 -0700

    Completed fib

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let n = Number(num) 
+    if (n < 0){
+        return "OOPS"
+    }
+    if (n <= 1){
+        return n;
+    }
+    else{
+        return fibonacci(n-1) + fibonacci(n -2);
+    }
 
 };
 
