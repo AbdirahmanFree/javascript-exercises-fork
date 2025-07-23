1dbaa5fc17f632eb52b14f3a725f651adf27dc8b
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-23 16:07:55 -0700

    complete findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest ="" ;
+    let oldestAge = 0;
+    for(person of people){
+        let age = 0;
+        if(person.yearOfDeath == undefined){
+            age = 2025  - person.yearOfBirth;
+        }
+        else{
+            age = person.yearOfDeath  -person.yearOfBirth;
+        }
+
+        if (age >oldestAge){
+            oldestAge = age;
+            oldest = person;
+        }
+    }
+    return oldest;
 
 };
 
