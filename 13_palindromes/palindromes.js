11c2cd6cd11525afa6aed52e2fb89df5a04d93ec
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-23 14:59:36 -0700

    Completed palindrome checker using 2 pointers

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,55 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringNew = string.toLowerCase()
+    let frontPointer = 0;
+    let endPointer = stringNew.length -1;
+    while (frontPointer< endPointer){
+        function isAlpha(ch){
+            if((ch.charCodeAt(0)>= 97 && ch.charCodeAt(0) <=122) || (ch.charCodeAt(0) >=65 && ch.charCodeAt(0) <= 91)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        function isPunc(ch){
+            if(ch == '.' || ch == ',' || ch == '!' || ch == ' '){
+                return true;
+            }
+            else {
+                false;
+            }
+        }
+        while(frontPointer < endPointer){
+            if(isPunc(stringNew.charAt(frontPointer)) && isPunc(stringNew.charAt(endPointer))){
+                frontPointer +=1;
+                endPointer -=1;
+                continue;
+            }
+            if(isPunc(stringNew.charAt(frontPointer))){
+                frontPointer +=1
+                continue;
+            }
+            if (isPunc(stringNew.charAt(endPointer))){
+                endPointer -=1
+                continue;
+            }
+            if(stringNew.charAt(frontPointer) == stringNew.charAt(endPointer)){
+                frontPointer +=1;
+                endPointer -=1;
+            }
+            else{
+                return false
+            }
+        }
+        return true;
+
+
+
+
+    
+    }
+    
 
 };
 
